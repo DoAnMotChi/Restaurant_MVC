3b6b86bff3ec1ea90bd24b6fa10f0324dcbbbcc6
Tran Kha <35250985+TranKhats@users.noreply.github.com>
2018-11-30 09:29:53 +0700

    xong

---
+++ Restaurant/Restaurant/Controllers/TableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult ChooseTable()
+        {
+            return View();
+        }
+    }
+}
